e2ef8a603314f0cad8bff28c3c772e3c0a64d96b
Sean Delaney <hello@delaneymethod.com>
2021-04-07 18:31:36 +0100

    Interceptors
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import './index.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+axios.interceptors.request.use(request => {
+	console.log(request);
+
+	return request;
+}, error => {
+	console.error(error);
+
+	return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+	console.log(response);
+
+	return response;
+}, error => {
+	console.error(error);
+
+	return Promise.reject(error);
+});
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
